b22ac1516f0a78dd3157265a5a4a9c60358b2def
Jonathan Carter <joncart@microsoft.com>
2020-09-11 21:41:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Codespaces Port
+
+If you're developing within a GitHub Codespace, and would like to retrieve the URL of a forwarded port, then simply run the following command, specifying the port number that you'd like to retrieve the URL for:
+
+`npx codespaces-port <port>`
---
+++ index.js
@@ -0,0 +1,15 @@
+if (!process.env["CODESPACES"]) {
+    console.log("This tool can only be run within the context of a GitHub Codespace: https://github.com/features/codespaces.");
+    return;
+}
+
+const port = process.argv[2];
+if (!port) {
+    console.log("Specify a port in order to retrieve it's URL: codespace-port <port>.");
+    return;
+}
+
+const id = process.env["CLOUDENV_ENVIRONMENT_ID"];
+const url = `https://${id}-${port}.apps.codespaces.githubusercontent.com`;
+
+console.log(url);
